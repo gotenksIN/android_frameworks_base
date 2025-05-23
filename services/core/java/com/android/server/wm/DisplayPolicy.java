09a23bd97be43b6a521ed4c048df6cb56a16fa46
Renuka Parlikar <quic_rparlika@quicinc.com>
2025-05-23 14:22:48 +0530

    Perf : Send activityname to perfhal
    Send activityName instead of appName to perfhal via perfhint for vendor hint scroll boost
    
    CRs-Fixed:4161386
    
    Change-Id: I140bfa4107e9d78158a32e6bc57edf13008982aa

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -455,6 +455,19 @@ public class DisplayPolicy {
         return currentPackage;
     }
 
+    private String getActivityName() {
+        String activityName;
+        try {
+            ActivityManager.RunningTaskInfo rti = ActivityTaskManager.getService().getTasks(
+                1, false /* filterVisibleRecents */, false /*keepIntentExtra */,
+                INVALID_DISPLAY).get(0);
+            activityName = rti.topActivity.flattenToShortString();
+        } catch (Exception e) {
+            activityName = null;
+        }
+        return activityName;
+    }
+
     private boolean isTopAppGame(String currentPackage, BoostFramework BoostType) {
         boolean isGame = false;
         if (isLowRAM) {
@@ -637,6 +650,11 @@ public class DisplayPolicy {
                             Slog.e(TAG, "Error: package name null");
                             return;
                         }
+                        String currentActivity = getActivityName();
+                        if (currentActivity == null) {
+                            Slog.e(TAG, "Error: activity name null");
+                            return;
+                        }
                         if (SCROLL_BOOST_SS_ENABLE) {
 // QTI_END: 2019-10-16: Performance: Added Workload to detect app type based on target
 // QTI_BEGIN: 2019-01-29: Core: Revert "Temporarily revert am, wm, and policy servers to upstream QP1A.181202.001"
@@ -652,9 +670,16 @@ public class DisplayPolicy {
 // QTI_BEGIN: 2019-10-16: Performance: Added Workload to detect app type based on target
                             boolean isGame = isTopAppGame(currentPackage, mPerfBoostFling);
                             if (!isGame) {
-                                mPerfBoostFling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
-                                    currentPackage, duration + 160, BoostFramework.Scroll.VERTICAL);
-                                mIsPerfBoostFlingAcquired = true;
+                                if (mPerfBoostFling.board_first_api_lvl <= BoostFramework.VENDOR_V_API_LEVEL &&
+                                    mPerfBoostFling.board_api_lvl <= BoostFramework.VENDOR_V_API_LEVEL) {
+                                        mPerfBoostFling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
+                                         currentPackage, duration + 160, BoostFramework.Scroll.VERTICAL);
+                                        mIsPerfBoostFlingAcquired = true;
+                                } else {
+                                        mPerfBoostFling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
+                                        currentActivity, duration + 160, BoostFramework.Scroll.VERTICAL);
+                                        mIsPerfBoostFlingAcquired = true;
+                                }
                            }
 // QTI_END: 2019-10-16: Performance: Added Workload to detect app type based on target
 // QTI_BEGIN: 2019-07-16: Performance: perf: Convert Horizontal Scroll to GestureFlingBoost.
@@ -668,6 +693,11 @@ public class DisplayPolicy {
                         String currentPackage = getAppPackageName();
                         if (currentPackage == null) {
                             Slog.e(TAG, "Error: package name null");
+                            return;
+                        }
+                        String currentActivity = getActivityName();
+                        if (currentActivity == null) {
+                            Slog.e(TAG, "Error: activity name null");
                             return;
                         }
                         if (SCROLL_BOOST_SS_ENABLE) {
@@ -685,9 +715,16 @@ public class DisplayPolicy {
 // QTI_BEGIN: 2019-10-16: Performance: Added Workload to detect app type based on target
                             boolean isGame = isTopAppGame(currentPackage, mPerfBoostFling);
                             if (!isGame) {
-                                mPerfBoostFling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
-                                    currentPackage, duration + 160, BoostFramework.Scroll.HORIZONTAL);
-                                mIsPerfBoostFlingAcquired = true;
+                                if (mPerfBoostFling.board_first_api_lvl <= BoostFramework.VENDOR_V_API_LEVEL &&
+                                    mPerfBoostFling.board_api_lvl <= BoostFramework.VENDOR_V_API_LEVEL) {
+                                        mPerfBoostFling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
+                                         currentPackage, duration + 160, BoostFramework.Scroll.HORIZONTAL);
+                                        mIsPerfBoostFlingAcquired = true;
+                                } else {
+                                        mPerfBoostFling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
+                                        currentActivity, duration + 160, BoostFramework.Scroll.HORIZONTAL);
+                                        mIsPerfBoostFlingAcquired = true;
+                                }
                             }
 // QTI_END: 2019-10-16: Performance: Added Workload to detect app type based on target
 // QTI_BEGIN: 2019-01-29: Core: Revert "Temporarily revert am, wm, and policy servers to upstream QP1A.181202.001"
@@ -701,6 +738,11 @@ public class DisplayPolicy {
                         String currentPackage = getAppPackageName();
                         if (currentPackage == null) {
                             Slog.e(TAG, "Error: package name null");
+                            return;
+                        }
+                        String currentActivity = getActivityName();
+                        if (currentActivity == null) {
+                            Slog.e(TAG, "Error: activity name null");
                             return;
                         }
                         boolean isGame;
@@ -729,8 +771,14 @@ public class DisplayPolicy {
 // QTI_BEGIN: 2019-10-16: Performance: Added Workload to detect app type based on target
                             isGame = isTopAppGame(currentPackage, mPerfBoostPrefling);
                             if (!isGame) {
-                                mPerfBoostPrefling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
+                                if (mPerfBoostPrefling.board_first_api_lvl <= BoostFramework.VENDOR_V_API_LEVEL &&
+                                    mPerfBoostPrefling.board_api_lvl <= BoostFramework.VENDOR_V_API_LEVEL) {
+                                        mPerfBoostPrefling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
                                         currentPackage, -1, BoostFramework.Scroll.PREFILING);
+                                } else {
+                                mPerfBoostPrefling.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST,
+                                        currentActivity, -1, BoostFramework.Scroll.PREFILING);
+                                }
                             }
 // QTI_END: 2019-10-16: Performance: Added Workload to detect app type based on target
 // QTI_BEGIN: 2019-01-29: Core: Revert "Temporarily revert am, wm, and policy servers to upstream QP1A.181202.001"

41fbf804ba112e212ac1561e0fd6a49bcb39e865
junklu <junklu@qti.qualcomm.com>
2025-12-11 15:13:32 +0800

    SystemUI: Refactor dual sim icon on status bar.
    Google enable stacked dual sim icons and refactor
    status bar icons by compose view.
    This change is to readapt HD icon feature.
    
    1. Add config to disable stacked dual sim icons
    2. Add HD icon in compose view
    
    Change-Id: I8ec526363718dc109ac633eb8f53456a4ddb750c
    CRs-Fixed: 4333553

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconsInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconsInteractor.kt
@@ -35,6 +35,9 @@ import com.android.systemui.flags.FeatureFlagsClassic
 import com.android.systemui.flags.Flags.FILTER_PROVISIONING_NETWORK_SUBSCRIPTIONS
 import com.android.systemui.log.table.TableLogBuffer
 import com.android.systemui.log.table.logDiffsForTable
+// QTI_BEGIN: 2025-12-16: Android_UI: SystemUI: Refactor dual sim icon on status bar.
+import com.android.systemui.res.R
+// QTI_END: 2025-12-16: Android_UI: SystemUI: Refactor dual sim icon on status bar.
 import com.android.systemui.statusbar.core.NewStatusBarIcons
 import com.android.systemui.statusbar.pipeline.dagger.MobileSummaryLog
 import com.android.systemui.statusbar.pipeline.mobile.data.model.SubscriptionModel
@@ -176,7 +179,10 @@ constructor(
     private val reuseCache = mutableMapOf<Int, WeakReference<MobileIconInteractor>>()
 // QTI_BEGIN: 2025-04-07: Android_UI: SystemUI: Readapt Mobile Icon Features For Kairos(1/2)
     private var ddsIcon: StateFlow<SignalIconModel?> = MutableStateFlow(null)
-
+// QTI_BEGIN: 2025-12-16: Android_UI: SystemUI: Refactor dual sim icon on status bar.
+    private val enableStackedIcon: Boolean =
+        context.resources.getBoolean(R.bool.config_enable_stacked_icon)
+// QTI_END: 2025-12-16: Android_UI: SystemUI: Refactor dual sim icon on status bar.
     override fun setDdsIconFLow(iconFlow: StateFlow<SignalIconModel?>) {
         ddsIcon = iconFlow
     }
@@ -315,7 +321,9 @@ constructor(
             }
             .stateIn(scope, SharingStarted.WhileSubscribed(), emptyList())
     override val isStackable =
-        if (NewStatusBarIcons.isEnabled) {
+// QTI_BEGIN: 2025-12-16: Android_UI: SystemUI: Refactor dual sim icon on status bar.
+        if (NewStatusBarIcons.isEnabled && enableStackedIcon) {
+// QTI_END: 2025-12-16: Android_UI: SystemUI: Refactor dual sim icon on status bar.
             icons.flatMapLatest { icons ->
                 if (icons.isEmpty()) {
                     flowOf(false)

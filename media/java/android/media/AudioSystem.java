d07eebbe51801b2b0d2281f442fbbc5cfdc3fa25
Mayank Madhukar <quic_mmadhuka@quicinc.com>
2026-04-24 15:38:31 +0530

    media: added case for Aptx Adaptive
    CRs-Fixed: 4476774
    
    Depends-On: I9b268d2acb29b3f4d63db292492a212ed7252326
    
    Change-Id: I3f7a9c1d8e4b2a6f9c5d0e7a1b4c8f2d6e9a0b3c

--- media/java/android/media/AudioSystem.java
+++ media/java/android/media/AudioSystem.java
@@ -384,6 +384,7 @@ public class AudioSystem
             case AUDIO_FORMAT_SBC -> BluetoothCodecType.CODEC_ID_SBC;
             case AUDIO_FORMAT_APTX -> BluetoothCodecType.CODEC_ID_APTX;
             case AUDIO_FORMAT_APTX_HD -> BluetoothCodecType.CODEC_ID_APTX_HD;
+            case AUDIO_FORMAT_APTX_ADAPTIVE -> BluetoothCodecType.CODEC_ID_APTX_AD;
             case AUDIO_FORMAT_LDAC -> {
                 if (com.android.bluetooth.flags.Flags.a2dpLdacApi()) {
                     yield BluetoothCodecType.CODEC_ID_SONY_LDAC;
